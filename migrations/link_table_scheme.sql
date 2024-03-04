489b90ae5cdec732ceb5bf41adcfe3d45794f95c
evgenij <chucknorristhegreat@yandex.ru>
2024-03-04 21:47:15 +0300

    finished hw4

--- migrations/link_table_scheme.sql
+++ migrations/link_table_scheme.sql
@@ -1,6 +1,6 @@
 create table if not exists link
 (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url TEXT UNIQUE,
     updated_at TIMESTAMP WITH TIME ZONE,
     checked_at TIMESTAMP WITH TIME ZONE
