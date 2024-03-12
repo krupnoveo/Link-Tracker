7b04806b8c905d09fbad203f984c0639147a5f0a
evgenij <chucknorristhegreat@yandex.ru>
2024-03-12 14:18:32 +0300

    added interface for interaction with chat_to_link table in db

---
+++ scrapper/src/main/java/edu/java/domain/ChatsToLinksDao.java
@@ -0,0 +1,17 @@
+package edu.java.domain;
+
+import edu.java.api.dto.response.LinkResponse;
+import java.net.URI;
+import java.util.List;
+
+public interface ChatsToLinksDao {
+    void add(long chatId, long linkId, URI uri);
+
+    void remove(long chatId, long linkId, URI uri);
+
+    List<LinkResponse> findAllByChatId(long chatId);
+
+    boolean isLinkTrackedByAnybody(long linkId);
+
+    List<LinkResponse> removeChatAndAllConnectedLinks(long chatId);
+}
