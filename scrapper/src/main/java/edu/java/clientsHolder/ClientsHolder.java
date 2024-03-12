a77ea38f3094e08ba78d8733442b936a3e9a2c22
evgenij <chucknorristhegreat@yandex.ru>
2024-03-12 14:15:43 +0300

    added handling for exceptions

--- scrapper/src/main/java/edu/java/clientsHolder/ClientsHolder.java
+++ scrapper/src/main/java/edu/java/clientsHolder/ClientsHolder.java
@@ -1,11 +1,13 @@
 package edu.java.clientsHolder;
 
+import edu.java.api.exceptions.InvalidUrlFormatException;
+import edu.java.api.exceptions.UnsupportedUrlException;
 import edu.java.clients.BaseClient;
 import edu.java.models.LinkData;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.util.List;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
@@ -20,14 +22,17 @@ public class ClientsHolder {
         this.clientList = clientList;
     }
 
-    @SneakyThrows
-    public LinkData checkURl(String stringUrl) {
-        URL url = new URI(stringUrl).toURL();
-        for (BaseClient client : clientList) {
-            if (client.isUrlSupported(url)) {
-                return client.checkURL(url);
+    public LinkData checkURl(URI uri) {
+        try {
+            URL url = uri.toURL();
+            for (BaseClient client : clientList) {
+                if (client.isUrlSupported(url)) {
+                    return client.checkURL(url);
+                }
             }
+            throw new UnsupportedUrlException(uri.toString());
+        } catch (MalformedURLException e) {
+            throw new InvalidUrlFormatException(uri.toString());
         }
-        return null;
     }
 }
