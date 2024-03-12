c99eeeab2b3e0568a21e33c80f040f070e69a74a
evgenij <chucknorristhegreat@yandex.ru>
2024-03-12 14:14:54 +0300

    added interaction with repositories

--- scrapper/src/main/java/edu/java/api/services/jdbc/JdbcChatService.java
+++ scrapper/src/main/java/edu/java/api/services/jdbc/JdbcChatService.java
@@ -1,17 +1,32 @@
 package edu.java.api.services.jdbc;
 
+import edu.java.api.dto.response.LinkResponse;
 import edu.java.api.services.ChatService;
+import edu.java.domain.repository.JdbcChatRepository;
+import edu.java.domain.repository.JdbcChatsToLinksRepository;
+import edu.java.domain.repository.JdbcLinksRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class JdbcChatService implements ChatService {
+    private final JdbcChatRepository jdbcChatRepository;
+    private final JdbcLinksRepository jdbcLinksRepository;
+    private final JdbcChatsToLinksRepository jdbcChatsToLinksRepository;
+
     public void registerChat(long chatId) {
         log.info("Registering chat...");
+        jdbcChatRepository.add(chatId);
     }
 
     public void deleteChat(long chatId) {
         log.info("Deleting chat...");
+        List<LinkResponse> notTrackedByAnyoneLinks = jdbcChatsToLinksRepository.removeChatAndAllConnectedLinks(chatId);
+        jdbcChatRepository.remove(chatId);
+        jdbcLinksRepository.removeAll(notTrackedByAnyoneLinks);
     }
 }
--- scrapper/src/main/java/edu/java/api/services/jdbc/JdbcLinksService.java
+++ scrapper/src/main/java/edu/java/api/services/jdbc/JdbcLinksService.java
@@ -4,35 +4,57 @@ import edu.java.api.dto.request.AddLinkRequest;
 import edu.java.api.dto.request.RemoveLinkRequest;
 import edu.java.api.dto.response.LinkResponse;
 import edu.java.api.dto.response.ListLinksResponse;
-import java.net.URI;
-import java.util.List;
 import edu.java.api.services.LinksService;
+import edu.java.clientsHolder.ClientsHolder;
+import edu.java.domain.repository.JdbcChatsToLinksRepository;
+import edu.java.domain.repository.JdbcLinksRepository;
+import edu.java.models.LinkData;
+import java.net.URI;
+import java.time.OffsetDateTime;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class JdbcLinksService implements LinksService {
+    private final JdbcLinksRepository jdbcLinksRepository;
+    private final JdbcChatsToLinksRepository jdbcChatsToLinksRepository;
+    private final ClientsHolder clientsHolder;
+
     @SneakyThrows
     public ListLinksResponse getTrackedLinks(long chatId) {
         log.info("Getting tracked links...");
-        return new ListLinksResponse(
-            List.of(
-                new LinkResponse(1, new URI("ya.ru")),
-                new LinkResponse(2, new URI("google.com"))
-            )
-        );
+        return new ListLinksResponse(jdbcChatsToLinksRepository.findAllByChatId(chatId));
     }
 
     public LinkResponse addLinkToTracking(long chatId, AddLinkRequest addLinkRequest) {
         log.info("Adding link...");
-        return new LinkResponse(1L, addLinkRequest.link());
+        URI uri = addLinkRequest.link();
+        long linkId = jdbcLinksRepository.add(uri, getOffsetDateTimeFromLink(uri), OffsetDateTime.now());
+        jdbcChatsToLinksRepository.add(chatId, linkId, uri);
+        return new LinkResponse(linkId, uri);
     }
 
     @SneakyThrows
     public LinkResponse removeLinkFromTracking(long chatId, RemoveLinkRequest removeLinkRequest) {
         log.info("Deleting link...");
-        return new LinkResponse(removeLinkRequest.linkId(), new URI("test"));
+        long linkId = removeLinkRequest.linkId();
+        URI uri = jdbcLinksRepository.getUriById(linkId);
+        jdbcChatsToLinksRepository.remove(chatId, linkId, uri);
+        if (!jdbcChatsToLinksRepository.isLinkTrackedByAnybody(linkId)) {
+            jdbcLinksRepository.remove(linkId);
+        }
+        return new LinkResponse(linkId, uri);
+    }
+
+    private OffsetDateTime getOffsetDateTimeFromLink(URI uri) {
+        LinkData linkData = clientsHolder.checkURl(uri);
+        if (linkData.url() != null && linkData.lastUpdated() != null) {
+            return linkData.lastUpdated();
+        }
+        return null;
     }
 }
