06ec8a7008fb274f4ff03d1abffccd3267f3e4ee
evgenij <chucknorristhegreat@yandex.ru>
2024-03-13 22:30:49 +0300

    added json parser exception for handling

--- bot/src/main/java/edu/java/bot/api/controllers/ControllerAdvice.java
+++ bot/src/main/java/edu/java/bot/api/controllers/ControllerAdvice.java
@@ -1,5 +1,6 @@
 package edu.java.bot.api.controllers;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import edu.java.bot.api.dto.response.ApiErrorResponse;
 import edu.java.bot.api.exceptions.IncorrectRequestParametersException;
 import java.util.Arrays;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
-    @ExceptionHandler(IncorrectRequestParametersException.class)
+    @ExceptionHandler({IncorrectRequestParametersException.class, JsonParseException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrorResponse incorrectRequest(IncorrectRequestParametersException e) {
         return new ApiErrorResponse(
