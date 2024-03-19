a4a0a77a4900f10ad3081aa02c9978798f0401e4
evgenij <chucknorristhegreat@yandex.ru>
2024-03-19 12:30:59 +0300

    added configuration for jooq in order for him not to add quotes

--- scrapper/src/main/java/edu/java/configuration/BeansConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/BeansConfiguration.java
@@ -2,6 +2,9 @@ package edu.java.configuration;
 
 import java.util.Properties;
 import lombok.SneakyThrows;
+import org.jooq.conf.RenderQuotedNames;
+import org.jooq.impl.DefaultConfiguration;
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,4 +17,13 @@ public class BeansConfiguration {
         properties.load(getClass().getResourceAsStream("/messages.properties"));
         return properties;
     }
+
+    @Bean
+    public DefaultConfigurationCustomizer postgresJooqCustomizer() {
+        return (DefaultConfiguration c) -> c.settings()
+            .withRenderSchema(false)
+            .withRenderFormatted(true)
+            .withRenderQuotedNames(RenderQuotedNames.NEVER);
+    }
+
 }
